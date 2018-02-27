58eea3015f77f0b9aeef9466251a3659fe71ae06
zdiawara <zakaridiadiawara@gmail.com>
2018-02-27 20:52:37 +0100

    first commit

---
+++ src/assets/js/diapo.js
@@ -0,0 +1,16 @@
+function imageGalerie()
+{
+    var active = $('#galerie .active');
+     
+    var next = (active.next().length > 0) ? active.next() : $('#galerie img:first');
+     
+       active.fadeOut(function(){
+         
+       active.removeClass('active');
+       next.fadeln().addClass('active');  
+        
+       }); 
+}
+ 
+setInterval('imageGalerie()',2500);
+		
